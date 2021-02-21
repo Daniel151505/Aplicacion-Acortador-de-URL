a2652d46557e29ca02bdff0c718211c48b266379
Daniel151505 <daniel150520011@gmail.com>
2021-02-21 18:46:19 -0500

    Agregando Componente Spinner

--- Aplicacion-Acortador-de-URL/src/app/app.module.ts
+++ Aplicacion-Acortador-de-URL/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AcortadorUrlComponent } from './components/acortador-url/acortador-url.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AcortadorUrlComponent
+    AcortadorUrlComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
