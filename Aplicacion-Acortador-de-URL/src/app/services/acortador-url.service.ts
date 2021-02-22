2087180d7cdaed2007e6c7ad2f68f4fb9303433f
Daniel151505 <daniel150520011@gmail.com>
2021-02-22 13:38:08 -0500

    Configurando Acortador Service

--- Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
+++ Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
@@ -8,19 +8,19 @@ import { Observable } from 'rxjs';
 export class AcortadorUrlService {
 
   url='https://api-ssl.bitly.com/v4/shorten'
-  token = '00bdb4bae0ccde68c0ba65b97c6b10c623f21918'
+  
   
   constructor(private http:HttpClient) { }
 
   getUrlAcortador(nombreURL: string): Observable<any>{
    
-    const tokenHeader = new HttpHeaders({Authorization: 'Bearer' + this.token})
+    /* const tokenHeader = new HttpHeaders({Authorization: 'Bearer' + this.token}) */
 
     const body = {
       long_url: nombreURL
     }
 
-    return this.http.post(this.url, body ,{headers: tokenHeader})
+    return this.http.post(this.url, body)
   }
 
 }
