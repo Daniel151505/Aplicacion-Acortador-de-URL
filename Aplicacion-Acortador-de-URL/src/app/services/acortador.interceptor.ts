23e6d1bf50d34465f0e0b00ffaaba458163e4941
Daniel151505 <daniel150520011@gmail.com>
2021-02-22 13:35:36 -0500

    Interceptor

--- Aplicacion-Acortador-de-URL/src/app/services/acortador.interceptor.ts
+++ Aplicacion-Acortador-de-URL/src/app/services/acortador.interceptor.ts
@@ -3,9 +3,11 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AcortadorInterceptor implements HttpInterceptor {
@@ -13,6 +15,11 @@ export class AcortadorInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const TOKEN = '00bdb4bae0ccde68c0ba65b97c6b10c623f21918';
+
+    request = request.clone({ setHeaders: {Authorization: 'Bearer '+ TOKEN} })
+    return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
+      return throwError(error);
+    }));
   }
 }
