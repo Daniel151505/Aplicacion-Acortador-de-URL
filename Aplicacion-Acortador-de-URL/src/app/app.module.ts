e33c45ff633fd83b61f502f390845c13ede9cc1a
Daniel151505 <daniel150520011@gmail.com>
2021-02-21 19:08:55 -0500

    Importando FormsModule

--- Aplicacion-Acortador-de-URL/src/app/app.module.ts
+++ Aplicacion-Acortador-de-URL/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AcortadorUrlComponent } from './components/acortador-url/acortador-url.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
