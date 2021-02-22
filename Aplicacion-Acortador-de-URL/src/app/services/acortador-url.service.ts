e010ac9cd46ec8f2b849200ccc787fdaeb11654c
Daniel151505 <daniel150520011@gmail.com>
2021-02-21 19:59:25 -0500

    Importando HttpClient

--- Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
+++ Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,5 +9,5 @@ export class AcortadorUrlService {
   url='https://api-ssl.bitly.com/v4/shorten'
   token = '00bdb4bae0ccde68c0ba65b97c6b10c623f21918'
   
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
