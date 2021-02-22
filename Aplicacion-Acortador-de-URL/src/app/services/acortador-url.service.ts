3934703053abe43c594b1c8853c0467869e5a354
Daniel151505 <daniel150520011@gmail.com>
2021-02-21 19:54:20 -0500

    Agregando Variables

--- Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
+++ Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
@@ -5,5 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class AcortadorUrlService {
 
+  url='https://api-ssl.bitly.com/v4/shorten'
+  token = '00bdb4bae0ccde68c0ba65b97c6b10c623f21918'
+  
   constructor() { }
 }
