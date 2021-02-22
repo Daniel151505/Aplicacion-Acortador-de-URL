1f6e5b1a61a8211b783f834560114deab8e9f86b
Daniel151505 <daniel150520011@gmail.com>
2021-02-21 20:08:52 -0500

    Agregando Metodo getUrlAcortador

--- Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
+++ Aplicacion-Acortador-de-URL/src/app/services/acortador-url.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,16 @@ export class AcortadorUrlService {
   token = '00bdb4bae0ccde68c0ba65b97c6b10c623f21918'
   
   constructor(private http:HttpClient) { }
+
+  getUrlAcortador(nombreURL: string): Observable<any>{
+   
+    const tokenHeader = new HttpHeaders({Authorization: 'Bearer' + this.token})
+
+    const body = {
+      long_url: nombreURL
+    }
+
+    return this.http.post(this.url, body ,{headers: tokenHeader})
+  }
+
 }
